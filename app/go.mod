bf3b818c2836b1acb534e73dd06bb8a6513c9486
Emil Natan <e@foowatch.com>
2022-07-09 18:37:49 +0300

    Added modules

---
+++ app/go.mod
@@ -0,0 +1,15 @@
+module example.com/m/v2
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/oklahomer/go-kasumi v0.0.0-20220203122045-3db87696aa9c // indirect
+	github.com/oklahomer/go-sarah/v4 v4.0.3 // indirect
+	github.com/oklahomer/golack/v2 v2.1.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/tidwall/gjson v1.10.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
--- app/main.go
+++ app/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/oklahomer/go-sarah/v4"
+	"github.com/oklahomer/go-sarah/v4/slack"
 )
 
 func main() {
@@ -12,4 +15,23 @@ func main() {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func setupSlack() {
+	// Set up slack adapter.
+	slackConfig := slack.NewConfig()
+	slackConfig.Token = "REPLACE THIS"
+	adapter, err := slack.NewAdapter(slackConfig, slack.WithRTMPayloadHandler(slack.DefaultRTMPayloadHandler))
+	if err != nil {
+		panic(fmt.Errorf("faileld to setup Slack Adapter: %s", err.Error()))
+	}
+
+	// Set up an optional storage so conversational contexts can be stored.
+	cacheConfig := sarah.NewCacheConfig()
+	storage := sarah.NewUserContextStorage(cacheConfig)
+
+	// Set up a bot with Slack adapter and a default storage.
+	bot := sarah.NewBot(adapter, sarah.BotWithStorage(storage))
+
+	sarah.RegisterBot(bot)
 }
